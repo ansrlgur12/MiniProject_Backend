d6a7908241e96d3fb9c9a180f53fb12ac546b8da
JSH1990 <wjdtmdgus321@gamil.com>
2023-05-02 15:11:52 +0900

    JS

---
+++ src/main/java/com/chicchic/mini_project/vo/PerfumeVO.java
@@ -0,0 +1,15 @@
+package com.chicchic.mini_project.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Date;
+import java.util.List;
+
+@ToString
+@Getter
+@Setter
+public class PerfumeVO {
+    private String name;
+}
