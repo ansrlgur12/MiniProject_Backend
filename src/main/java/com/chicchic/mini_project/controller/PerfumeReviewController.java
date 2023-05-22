dd43c311bcc85e2b224ea53b247f6e90d8a2430c
jshblack <118979741+JSH1990@users.noreply.github.com>
2023-05-22 19:47:28 +0900

    Update PerfumeReviewController.java

--- src/main/java/com/chicchic/mini_project/controller/PerfumeReviewController.java
+++ src/main/java/com/chicchic/mini_project/controller/PerfumeReviewController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/reviews")
 public class PerfumeReviewController {
@@ -18,23 +19,42 @@ public class PerfumeReviewController {
         this.perfumeReviewDAO = perfumeReviewDAO;
     }
 
-    @RestController
-    public class ReviewController {
+    @PostMapping
+    public ResponseEntity<Boolean> newReview(@RequestBody Map<String, String> regData) {
+        int getperfumeNumber = Integer.parseInt(regData.get("perfumeNumber"));
+        String getuserId = regData.get("userId");
+        int getstarRating = Integer.parseInt(regData.get("starRating"));
+        String getreview = regData.get("review");
+        boolean isTrue = perfumeReviewDAO.newReview(getperfumeNumber, getuserId, getstarRating, getreview);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
+    }
 
-        @PostMapping("/reviews")
-        public ResponseEntity<Boolean> newReview(@RequestBody Map<String, String> regData) {
-            int getperfumeNumber = Integer.parseInt(regData.get("perfumeNumber"));
-            String getuserId = regData.get("userId");
-            int getstarRating = Integer.parseInt(regData.get("starRating"));
-            String getreview = regData.get("review");
-            boolean isTrue = perfumeReviewDAO.newReview(getperfumeNumber, getuserId, getstarRating, getreview);
-            return new ResponseEntity<>(isTrue, HttpStatus.OK);
-        }
+    @GetMapping("/{perfumeNumber}")
+    public List<PerfumeReviewVO> viewReview(@PathVariable int perfumeNumber) {
+        return perfumeReviewDAO.viewReview(perfumeNumber);
+    }
 
+   /* @PutMapping("/{perfumeNumber}/{userId}")
+    public ResponseEntity<Boolean> updateReview(@PathVariable int perfumeNumber, @PathVariable String userId, @RequestBody Map<String, String> updateData) {
+        int starRating = Integer.parseInt(updateData.get("starRating"));
+        String review = updateData.get("review");
 
-        @GetMapping("/reviews/{perfumeNumber}")
-        public List<PerfumeReviewVO> viewReview(@PathVariable int perfumeNumber) {
-            return perfumeReviewDAO.viewReview(perfumeNumber);
+        boolean isUpdated = perfumeReviewDAO.updateReview(perfumeNumber, userId, starRating, review);
+        return new ResponseEntity<>(isUpdated, HttpStatus.OK);
+    }*/
+
+    @DeleteMapping("/{perfumeNumber}/{userId}")
+    public ResponseEntity<Boolean> deleteReview(@PathVariable int perfumeNumber, @PathVariable String userId) {
+        boolean isDeleted = perfumeReviewDAO.deleteReview(perfumeNumber, userId);
+        return new ResponseEntity<>(isDeleted, HttpStatus.OK);
+    }
+    @GetMapping("/viewReview/{perfumeNumber}/{userId}")
+    public ResponseEntity<PerfumeReviewVO> viewReviewBeforeUpdate(@PathVariable int perfumeNumber, @PathVariable String userId) {
+        PerfumeReviewVO review = perfumeReviewDAO.viewReviewBeforeUpdate(perfumeNumber, userId);
+        if (review == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(review, HttpStatus.OK);
         }
     }
 
