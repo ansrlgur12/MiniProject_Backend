854755efff48c3a197c4d1187168070dcf40ac34
jshblack <118979741+JSH1990@users.noreply.github.com>
2023-05-22 19:31:37 +0900

    JSH

--- src/main/java/com/chicchic/mini_project/dao/PerfumeReviewDAO.java
+++ src/main/java/com/chicchic/mini_project/dao/PerfumeReviewDAO.java
@@ -3,11 +3,12 @@ package com.chicchic.mini_project.dao;
 import com.chicchic.mini_project.common.Common;
 import com.chicchic.mini_project.vo.PerfumeReviewVO;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @Repository
 public class PerfumeReviewDAO {
 
@@ -16,26 +17,35 @@ public class PerfumeReviewDAO {
     private ResultSet rs = null;
     private PreparedStatement pStmt = null;
 
-    public boolean newReview(int perfumeNumber, String userId, int starRating, String review) { //평점 넣기 메서드
+    public boolean newReview(int perfumeNumber, String userId, int starRating, String review) {
         int result = 0;
 
-        String sql = "INSERT INTO 한줄평 VALUES ( ?, " +
-                " (SELECT 회원번호 " +
-                " FROM 회원 " +
-                " WHERE 아이디 = ? ), ?, ? ) ";
+        String checkDuplicateSql = "SELECT COUNT(*) FROM 한줄평 WHERE 향수번호 = ? AND 회원번호 = (SELECT 회원번호 FROM 회원 WHERE 아이디 = ?)";
+        String insertSql = "INSERT INTO 한줄평 VALUES (?, (SELECT 회원번호 FROM 회원 WHERE 아이디 = ?), ?, ?)";
 
         try {
             conn = Common.getConnection();
-            pStmt = conn.prepareStatement(sql);
+            pStmt = conn.prepareStatement(checkDuplicateSql);
+            pStmt.setInt(1, perfumeNumber);
+            pStmt.setString(2, userId);
+            rs = pStmt.executeQuery();
+
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                if (count > 0) {
+                    // 이미 해당 사용자의 평점이 존재하므로 중복으로 처리
+                    return false;
+                }
+            }
+
+            pStmt = conn.prepareStatement(insertSql);
             pStmt.setInt(1, perfumeNumber);
             pStmt.setString(2, userId);
             pStmt.setInt(3, starRating);
             pStmt.setString(4, review);
 
             result = pStmt.executeUpdate();
-            System.out.println("평점 등록 DB 결과 확인 : " + result);
-
-
+            System.out.println("평점 등록 DB 결과 확인: " + result);
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -47,14 +57,14 @@ public class PerfumeReviewDAO {
                 e.printStackTrace();
             }
         }
-        if (result == 1) return true;
-        else return false;
+
+        return result == 1;
     }
 
     public List<PerfumeReviewVO> viewReview(int perfumeNumber) {
         List<PerfumeReviewVO> list = new ArrayList<>();
 
-        String sql = "SELECT * FROM 한줄평 WHERE 향수번호 = ?";
+        String sql = "SELECT 한줄평.*, 회원.아이디, 회원.이미지 FROM 한줄평 JOIN 회원 ON 한줄평.회원번호 = 회원.회원번호 WHERE 한줄평.향수번호= ?";
 
         try {
             conn = Common.getConnection();
@@ -64,14 +74,15 @@ public class PerfumeReviewDAO {
 
             while (rs.next()) {
                 int perfumeNum = rs.getInt("향수번호");
-                String userId = rs.getString("회원번호");
+                String userId = rs.getString("아이디");
                 int starRating = rs.getInt("별점");
                 String review = rs.getString("한줄평");
+                String image = rs.getString("이미지");
 
-                // 출력하기 전
-                System.out.println("Before inserting into VO - PerfumeNum: " + perfumeNumber + ", UserId: " + userId + ", StarRating: " + starRating + ", Review: " + review);
+                // 출력 전
+                System.out.println("Before inserting into VO - PerfumeNum: " + perfumeNumber + ", UserId: " + userId + ", StarRating: " + starRating + ", Review: " + review + ",image" + image);
 
-                PerfumeReviewVO vo = new PerfumeReviewVO(perfumeNumber, userId, starRating, review);
+                PerfumeReviewVO vo = new PerfumeReviewVO(perfumeNumber, userId, starRating, review, image);
                 list.add(vo);
 
                 // 출력한 후
@@ -93,5 +104,94 @@ public class PerfumeReviewDAO {
         return list;
     }
 
+   /* public boolean updateReview(int perfumeNumber, String userId, int starRating, String review) {
+        int result = 0;
 
+        String sql = "UPDATE 한줄평 SET 별점 = ?, 한줄평 = ? WHERE 향수번호 = ? AND 회원번호 = (SELECT 회원번호 FROM 회원 WHERE 아이디 = ?)";
+
+        try {
+            conn = Common.getConnection();
+            pStmt = conn.prepareStatement(sql);
+            pStmt.setInt(1, starRating);
+            pStmt.setString(2, review);
+            pStmt.setInt(3, perfumeNumber);
+            pStmt.setString(4, userId);
+
+            result = pStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pStmt != null) pStmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return result == 1;
+    }*/
+
+    public boolean deleteReview(int perfumeNumber, String userId) {
+        int result = 0;
+
+        String sql = "DELETE FROM 한줄평 WHERE 향수번호 = ? AND 회원번호 = (SELECT 회원번호 FROM 회원 WHERE 아이디 = ?)";
+
+        try {
+            conn = Common.getConnection();
+            pStmt = conn.prepareStatement(sql);
+            pStmt.setInt(1, perfumeNumber);
+            pStmt.setString(2, userId);
+
+            result = pStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (rs != null) rs.close();
+                if (pStmt != null) pStmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result == 1;
+    }
+
+    public PerfumeReviewVO viewReviewBeforeUpdate(int perfumeNumber, String userId) {
+        PerfumeReviewVO review = null;
+
+        String sql = "SELECT 한줄평.*, 회원.아이디, 회원.이미지 FROM 한줄평 JOIN 회원 ON 한줄평.회원번호 = 회원.회원번호 WHERE 한줄평.향수번호= ? AND 회원.아이디 = ?";
+
+        try {
+            conn = Common.getConnection();
+            pStmt = conn.prepareStatement(sql);
+            pStmt.setInt(1, perfumeNumber);
+            pStmt.setString(2, userId);
+            rs = pStmt.executeQuery();
+
+            if (rs.next()) {
+                int starRating = rs.getInt("별점");
+                String reviewContent = rs.getString("한줄평");
+                String image = rs.getString("이미지");
+
+                review = new PerfumeReviewVO(perfumeNumber, userId, starRating, reviewContent, image);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pStmt != null) pStmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return review;
+    }
 }
--- src/main/java/com/chicchic/mini_project/vo/PerfumeReviewVO.java
+++ src/main/java/com/chicchic/mini_project/vo/PerfumeReviewVO.java
@@ -5,12 +5,15 @@ public class PerfumeReviewVO {
     private String userId;
     private int starRating;
     private String review;
+    private String image;
 
-    public PerfumeReviewVO(int perfumeNum, String userId, int starRating, String review) {
+    public PerfumeReviewVO(int perfumeNum, String userId, int starRating, String review, String image) {
         this.perfumeNumber = perfumeNum;
         this.userId = userId;
         this.starRating = starRating;
         this.review = review;
+        this.image = image;
+
     }
 
 
@@ -21,6 +24,7 @@ public class PerfumeReviewVO {
                 ", userId='" + userId + '\'' +
                 ", starRating=" + starRating +
                 ", review='" + review + '\'' +
+                ", image='" + image + '\'' +
                 '}';
     }
 
@@ -58,4 +62,6 @@ public class PerfumeReviewVO {
     public void setReview(String review) {
         this.review = review;
     }
+    public String getImage() {return  image;}
+    public  void  setImage(String image) {this.image = image;}
 }
